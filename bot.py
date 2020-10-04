152f67367f8e2866e3bda2857de84f6954f2e981
Andruida <borus.benedek@gmail.com>
2020-10-05 01:21:41 +0200

    Finished the embed design and introduced message.ini as langfile

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 import logging
 import configparser
 import requests
+import datetime
 
 import traceback
 
@@ -16,6 +17,9 @@ logger.addHandler(handler)
 config = configparser.ConfigParser()
 
 config.read("config.ini")
+
+messages = configparser.ConfigParser()
+messages.read("messages.ini")
 
 bot = commands.Bot(command_prefix=config["discord"]["prefix"])
 bot.remove_command('help')
@@ -32,6 +36,7 @@ async def on_ready():
 	print("Finished loading, ready for operation.")
 
 @commands.guild_only()
+@commands.has_guild_permissions(administrator=True)
 @bot.command()
 async def status(ctx):
 	global status_message
@@ -46,18 +51,72 @@ async def update_status():
 	global status_message
 	await bot.wait_until_ready()
 	print(bot.is_closed())
-	last_number = -1
+	last_embed = None
+	last_update = datetime.datetime.now()
 	while not bot.is_closed():
-		print("run")
 		if status_message:
-			print("message ready")
-			r = requests.get("http://fivem.hlrp.hu:30120/players.json")
-			data = r.json()
-			if last_number != len(data):
-				last_number = len(data)
-				print("updated")
-				await status_message.edit(content="Online játékosok {}/64.".format(last_number))
-		await asyncio.sleep(30)
+			messages.read("messages.ini")
+			online = False
+			dev_online = False
+			try:
+				r = requests.get(config["server"].get("main", "http://localhost:30120/players.json"))
+				data = r.json()
+				online = True
+			except:
+				data = []
+				online = False
+			try:
+				r = requests.get(config["server"].get("dev", "http://localhost:30120/players.json"))
+				dev_online = True
+			except:
+				dev_online = False
+			embed = content_generator(online, data, last_update, dev_online)
+			if last_embed != embed.to_dict():
+				print("updated:", len(data), online, dev_online)
+				last_update = datetime.datetime.now()
+				embed = content_generator(online, data, last_update, dev_online)
+				last_embed = embed.to_dict()
+				await status_message.edit(content="", embed=embed)
+		await asyncio.sleep(5)
+
+def content_generator(online,data, last_update, dev_online=True):
+	if online:
+		color = 0x18DC56
+	else:
+		color = 0xE03F3F
+
+	unknown = messages["global"].get("unknown", "*Unknown*")
+	message = messages["message"]
+	title = messages["title"]
+
+	embed = discord.Embed(color=color, description=message.get("description", unknown)+"\n\u200b")
+	embed.set_author(name=message.get("name", unknown), url=message.get("url", discord.Embed.Empty), icon_url=message.get("icon", discord.Embed.Empty))
+
+
+	embed.add_field(name=title.get("specs", unknown), value=message.get("specs", unknown)+"\n\u200b", inline=False)
+	embed.add_field(name=title.get("ip", unknown), value=message.get("ip", unknown), inline=False)
+	embed.add_field(name=title.get("restart", unknown), value=message.get("restart", unknown)+"\n\u200b", inline=False)
+	embed.add_field(name=title.get("discord", unknown), value=message.get("discord", unknown), inline=False)
+	embed.add_field(name=title.get("web", unknown), value=message.get("web", unknown), inline=False)
+	embed.add_field(name=title.get("forum", unknown), value=message.get("forum", unknown), inline=False)
+	embed.add_field(name=title.get("facebook", unknown), value=message.get("facebook", unknown), inline=False)
+	embed.add_field(name=title.get("leaderboard", unknown), value=message.get("leaderboard", unknown)+"\n\u200b", inline=False)
+	embed.add_field(name=title.get("status", unknown), value=message.get("online", unknown) if online else message.get("offline", unknown), inline=True)
+	embed.add_field(name=title.get("online_players", unknown), value="{}/{}".format(len(data), message.get("online_max", -1)) if online else message.get("offline", unknown), inline=True)
+	embed.add_field(name=title.get("dev_status", unknown), value=message.get("online", unknown)+"\n\u200b" if dev_online else message.get("offline", unknown)+"\n\u200b", inline=True)
+
+	players = ["","",""]
+	data.sort(key=lambda x: x.get("name", unknown)[:20])
+	for p in data:
+		players[data.index(p)%3] += "{} [{}]\n".format(p.get("name", unknown), p.get("id", "-1"))
+	embed.add_field(name=title.get("players", unknown), value=players[0] if len(players[0]) > 0 else message.get("no_online_player", unknown), inline=True)
+	embed.add_field(name="\u200b", value=players[1] if len(players[1]) > 0 else "\u200b", inline=True)
+	embed.add_field(name="\u200b", value=players[2]+"\n\u200b" if len(players[2]) > 0 else "\u200b", inline=True)
+
+	embed.timestamp = last_update
+	embed.set_footer(text=message.get("name", unknown), icon_url=message.get("icon", discord.Embed.Empty))
+
+	return embed
 
 bot.loop.create_task(update_status())
 
